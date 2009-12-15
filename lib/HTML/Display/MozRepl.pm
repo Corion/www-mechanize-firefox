29ea90a07e57ff193ec32a69270c07a7e865ef67
Max Maischein <corion@corion.net>
2009-12-15 22:10:05 +0100

    Bump version numbers

--- lib/HTML/Display/MozRepl.pm
+++ lib/HTML/Display/MozRepl.pm
@@ -4,7 +4,7 @@ use Carp qw(carp);
 use WWW::Mechanize::FireFox;
 use parent 'HTML::Display::Common';
 use vars qw($VERSION);
-$VERSION = '0.13';
+$VERSION = '0.14';
 
 =head1 NAME
 
--- lib/HTTP/Cookies/MozRepl.pm
+++ lib/HTTP/Cookies/MozRepl.pm
@@ -5,7 +5,7 @@ use parent 'HTTP::Cookies';
 use Carp qw[croak];
 
 use vars qw[$VERSION];
-$VERSION = '0.13';
+$VERSION = '0.14';
 
 =head1 NAME
 
