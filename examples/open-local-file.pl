1069c411f654dbab59a6ed3201246dfc2a1ae093
Max Maischein <corion@corion.net>
2010-05-30 17:37:23 +0200

    Auto-generate README, README.mkdn, examples+example listing

---
+++ examples/open-local-file.pl
@@ -0,0 +1,7 @@
+use strict;
+use WWW::Mechanize::FireFox;
+
+my $mech = WWW::Mechanize::FireFox->new();
+$mech->get_local('datei.html');
+
+<>;
---
+++ examples/open-url.pl
@@ -0,0 +1,7 @@
+use strict;
+use WWW::Mechanize;
+
+my $mech = WWW::Mechanize::FireFox->new();
+$mech->get('http://www.perlworkshop.de');
+
+<>;
