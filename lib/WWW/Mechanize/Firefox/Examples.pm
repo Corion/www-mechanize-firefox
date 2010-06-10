6a36826b7dcc979484847dfb2713a8c4164c828c
Max Maischein <corion@corion.net>
2010-06-10 21:21:38 +0200

    Version 0.19 for the examples too

--- lib/WWW/Mechanize/Firefox/Examples.pm
+++ lib/WWW/Mechanize/Firefox/Examples.pm
@@ -68,7 +68,7 @@ The following is a list of the 7 example programs that are included in the WWW::
     
     <>;
 
-Download this example: L<http://cpansearch.perl.org/src/CORION/WWW-Mechanize-Firefox-0.18/examples/open-local-file.pl>
+Download this example: L<http://cpansearch.perl.org/src/CORION/WWW-Mechanize-Firefox-0.19/examples/open-local-file.pl>
 
 =head2 Example: open-url.pl
 
@@ -80,7 +80,7 @@ Download this example: L<http://cpansearch.perl.org/src/CORION/WWW-Mechanize-Fir
     
     <>;
 
-Download this example: L<http://cpansearch.perl.org/src/CORION/WWW-Mechanize-Firefox-0.18/examples/open-url.pl>
+Download this example: L<http://cpansearch.perl.org/src/CORION/WWW-Mechanize-Firefox-0.19/examples/open-url.pl>
 
 =head2 Example: screenshot.pl
 
@@ -151,7 +151,7 @@ Download this example: L<http://cpansearch.perl.org/src/CORION/WWW-Mechanize-Fir
     
     =cut
 
-Download this example: L<http://cpansearch.perl.org/src/CORION/WWW-Mechanize-Firefox-0.18/examples/screenshot.pl>
+Download this example: L<http://cpansearch.perl.org/src/CORION/WWW-Mechanize-Firefox-0.19/examples/screenshot.pl>
 
 =head2 Example: dump-links.pl
 
@@ -169,7 +169,7 @@ Download this example: L<http://cpansearch.perl.org/src/CORION/WWW-Mechanize-Fir
     
     <>;
 
-Download this example: L<http://cpansearch.perl.org/src/CORION/WWW-Mechanize-Firefox-0.18/examples/dump-links.pl>
+Download this example: L<http://cpansearch.perl.org/src/CORION/WWW-Mechanize-Firefox-0.19/examples/dump-links.pl>
 
 =head2 Example: manipulate-javascript.pl
 
@@ -192,7 +192,7 @@ Download this example: L<http://cpansearch.perl.org/src/CORION/WWW-Mechanize-Fir
     
     <>;
 
-Download this example: L<http://cpansearch.perl.org/src/CORION/WWW-Mechanize-Firefox-0.18/examples/manipulate-javascript.pl>
+Download this example: L<http://cpansearch.perl.org/src/CORION/WWW-Mechanize-Firefox-0.19/examples/manipulate-javascript.pl>
 
 =head2 Example: javascript.pl
 
@@ -208,7 +208,7 @@ Download this example: L<http://cpansearch.perl.org/src/CORION/WWW-Mechanize-Fir
     
     <>;
 
-Download this example: L<http://cpansearch.perl.org/src/CORION/WWW-Mechanize-Firefox-0.18/examples/javascript.pl>
+Download this example: L<http://cpansearch.perl.org/src/CORION/WWW-Mechanize-Firefox-0.19/examples/javascript.pl>
 
 =head2 Example: urlbar.pl
 
@@ -319,7 +319,7 @@ Download this example: L<http://cpansearch.perl.org/src/CORION/WWW-Mechanize-Fir
     };
 
 
-Download this example: L<http://cpansearch.perl.org/src/CORION/WWW-Mechanize-Firefox-0.18/examples/urlbar.pl>
+Download this example: L<http://cpansearch.perl.org/src/CORION/WWW-Mechanize-Firefox-0.19/examples/urlbar.pl>
 
 =head1 AUTHOR
 
