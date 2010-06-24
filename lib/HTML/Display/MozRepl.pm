3afa9af62600c21b2a072dc05cb2877261bfc075
Max Maischein <corion@corion.net>
2010-06-24 19:49:24 +0200

    Bump versions, regenerate README

--- lib/HTML/Display/MozRepl.pm
+++ lib/HTML/Display/MozRepl.pm
@@ -4,7 +4,7 @@ use Carp qw(carp);
 use WWW::Mechanize::FireFox;
 use parent 'HTML::Display::Common';
 use vars qw($VERSION);
-$VERSION = '0.22';
+$VERSION = '0.23';
 
 =head1 NAME
 
--- lib/HTTP/Cookies/MozRepl.pm
+++ lib/HTTP/Cookies/MozRepl.pm
@@ -5,7 +5,7 @@ use parent 'HTTP::Cookies';
 use Carp qw[croak];
 
 use vars qw[$VERSION];
-$VERSION = '0.22';
+$VERSION = '0.23';
 
 =head1 NAME
 
