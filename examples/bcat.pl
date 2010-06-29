ffa71c80971a11e424f400255cee51b3fbcf808b
Max Maischein <corion@corion.net>
2010-06-29 18:53:59 +0200

    Add `bcat` utility

--- examples/bcat.pl
+++ examples/bcat.pl
@@ -5,7 +5,19 @@ use Getopt::Long;
 use Pod::Usage;
 use HTML::Display::MozRepl;
 
-my $d = HTML::Display::MozRepl->new();
+GetOptions(
+    'mozrepl|m:s' => \my $mozrepl,
+    'tab|t:s' => \my $tabname,
+    'close|c' => \my $close,
+    #'focus|f' => \my $focus,
+) or pod2usage();
+
+my $d = HTML::Display::MozRepl->new(
+    tabname => $tabname,
+    repl    => $mozrepl,
+    create  => 1,
+    autoclose => $close,
+);
 local $/;
 binmode STDIN;
 my $html = <>;
@@ -13,24 +25,29 @@ $d->display($html);
 
 =head1 NAME
 
-screenshot.pl - take a screenshot of a webpage
+bcat.pl - cat HTML to browser
 
 =head1 SYNOPSIS
 
-screenshot.pl [options] [url]
+bcat.pl <index.html
 
 Options:
-   --outfile        name of output file
+   --tabname        title of tab to reuse
    --mozrepl        connection string to Firefox
+   --close          automatically close the tab at the end of input
 
 =head1 OPTIONS
 
 =over 4
 
-=item B<--outfile>
+=item B<--tabname>
 
-Name of the output file. The image will always be written
-in PNG format.
+Name of the tab to (re)use. A substring is enough.
+
+=item B<--close>
+
+Automatically close the tab when the input closes. This is good
+for displaying intermediate information.
 
 =item B<--mozrepl>
 
@@ -40,8 +57,7 @@ Connection information for the mozrepl instance to use.
 
 =head1 DESCRIPTION
 
-B<This program> will take a screenshot
-of the given URL (including plugins) and
-write it to the given file or the file C<screenshot.png>.
+B<This program> will display HTML read from STDIN
+in a browser tab.
 
 =cut
