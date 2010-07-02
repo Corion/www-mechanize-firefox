ced231ff05ec7285ce8b4a8a63a2a34cc6d23f20
Max Maischein <corion@corion.net>
2010-07-02 14:39:53 +0200

    Regenerate examples

--- lib/WWW/Mechanize/Firefox/Examples.pm
+++ lib/WWW/Mechanize/Firefox/Examples.pm
@@ -38,7 +38,7 @@ program that is also included in the examples directory.
 
 =head1 Example programs
 
-The following is a list of the 9 example programs that are included in the WWW::Mechanize::Firefox distribution.
+The following is a list of the 11 example programs that are included in the WWW::Mechanize::Firefox distribution.
 
 =over
 
@@ -57,6 +57,10 @@ The following is a list of the 9 example programs that are included in the WWW::
 =item * L<Example: manipulate-javascript.pl> Make changes to Javascript values in a webpage
 
 =item * L<Example: javascript.pl> Execute Javascript in the webpage context
+
+=item * L<Example: js-console.pl> Send messages to the Error Console
+
+=item * L<Example: tail-console.pl> Display messages from the Error Console to STDOUT
 
 =item * L<Example: urlbar.pl> Listen to changes in the location bar
 
@@ -260,7 +264,7 @@ if (! $encode_type) {
     
     =head1 SYNOPSIS
     
-    bcat.pl <index.html
+      bcat.pl <index.html
     
     Options:
        --tabname        title of tab to reuse
@@ -412,6 +416,212 @@ Download this example: L<http://cpansearch.perl.org/src/CORION/WWW-Mechanize-Fir
     =cut
 
 Download this example: L<http://cpansearch.perl.org/src/CORION/WWW-Mechanize-Firefox-0.27/examples/javascript.pl>
+
+=head2 Example: js-console.pl
+
+    #!perl -w
+    use strict;
+    use lib 'C:/Projekte/MozRepl-RemoteObject/lib';
+    use WWW::Mechanize::Firefox;
+    use Time::HiRes;
+    use Getopt::Long;
+    use Pod::Usage;
+    
+    GetOptions(
+        'clear|c' => \my $clear,
+        'mozrepl|m:s' => \my $mozrepl,
+        'text|t:s' => \my $text,
+    ) or pod2usage();
+    
+    my $mech = WWW::Mechanize::Firefox->new(
+        #log => ['debug'],
+        mozrepl => $mozrepl,
+    );
+    
+    my $console = $mech->js_console;
+    
+    $mech->clear_js_errors
+        if ($clear);
+    
+    if ($text) {
+            $console->logStringMessage($text);
+    } else {
+        while (<>) {
+            $console->logStringMessage($_);
+        };
+    };
+    
+    =head1 NAME
+    
+    js-console.pl - send STDIN to the Javascript Console
+    
+    =head1 SYNOPSIS
+    
+        echo "Hello World" | js-console.pl
+    
+    Options:
+       --clear          Clear console before sending text
+       --mozrepl        connection string to Firefox
+       --close          automatically close the tab at the end of input
+       --type TYPE      Fix the type to 'html' or 'text'
+    
+    =head1 OPTIONS
+    
+    =over 4
+    
+    =item B<--clear>
+    
+    Clear the console before sending the text.
+    
+    =item B<--text TEXT>
+    
+    Send the text TEXT instead of reading from STDIN.
+    
+    =item B<--mozrepl>
+    
+    Connection information for the mozrepl instance to use.
+    
+    =back
+    
+    =head1 DESCRIPTION
+    
+    This program sends text read from standard input to the
+    Javascript Console in Firefox. This can be convenient
+    if you want to do testing and log the start or stop
+    of a test run to the console.
+    
+    =head1 SEE ALSO
+    
+    L<https://developer.mozilla.org/en/Error_Console>
+    
+    L<https://developer.mozilla.org/en/nsIConsoleService> - the underlying
+    Console Service that also shows how to listen to events getting
+    added.
+    
+    =cut
+
+Download this example: L<http://cpansearch.perl.org/src/CORION/WWW-Mechanize-Firefox-0.27/examples/js-console.pl>
+
+=head2 Example: tail-console.pl
+
+    #!perl -w
+    use strict;
+    use lib 'C:/Projekte/MozRepl-RemoteObject/lib';
+    use WWW::Mechanize::Firefox;
+    use Time::HiRes;
+    use Getopt::Long;
+    use Pod::Usage;
+    
+    GetOptions(
+        'mozrepl|m:s' => \my $mozrepl,
+        'follow|f'    => \my $install_listener,
+        'clear|c'     => \my $clear,
+        'number|n:i'  => \my $lines,
+    ) or pod2usage(2);
+    $lines ||= 5;
+    
+    my $mech = WWW::Mechanize::Firefox->new(
+        #log => ['debug'],
+        mozrepl => $mozrepl,
+    );
+    
+    sub install_listener {
+        warn "Creating logListener";
+        my $logListener = $mech->repl->declare(<<'JS');
+        function(callback) {
+            return {
+                observe: callback,
+                QueryInterface: function (iid) {
+                    if (!iid.equals(Components.interfaces.nsIConsoleListener) &&
+                        !iid.equals(Components.interfaces.nsISupports)) {
+                            throw Components.results.NS_ERROR_NO_INTERFACE;
+                    }
+                    return this;
+                },
+            };
+        }
+    JS
+    
+        warn "Creating registerListener";
+    
+        my $registerListener = $mech->repl->declare(<<'JS');
+            function (listener) {
+                var aConsoleService = Components.classes["@mozilla.org/consoleservice;1"]
+                    .getService(Components.interfaces.nsIConsoleService);
+                aConsoleService.registerListener(listener);
+            };
+    JS
+    
+        my $listener = $logListener->(sub {output_message($_[0])});
+        $registerListener->($listener);
+    };
+    
+    sub output_message {
+        print "$_[0]->{message}\n";
+    };
+    
+    my $console = $mech->js_console;
+    
+    $mech->clear_js_errors
+        if ($clear);
+    
+    output_message $_ for reverse (grep {defined} ($mech->js_errors)[-$lines..0]);
+    
+    if ($install_listener) {
+        my $l = install_listener;
+        while (1) {
+            $mech->repl->poll;
+            sleep 0.25;
+        };
+    };
+    
+    =head1 NAME
+    
+    js-console.pl - send STDIN to the Javascript Console
+    
+    =head1 SYNOPSIS
+    
+        tail-console.pl -f
+    
+    Options:
+       --clear          Clear console before receiving new messages
+       --follow         Read more messages as they are being added
+       --mozrepl        connection string to Firefox
+    
+    =head1 OPTIONS
+    
+    =over 4
+    
+    =item B<--clear>
+    
+    Clear the console before sending the text.
+    
+    =item B<--follow>
+    
+    Keep watching the console and output text as it gets added.
+    
+    =item B<--mozrepl>
+    
+    Connection information for the mozrepl instance to use.
+    
+    =back
+    
+    =head1 DESCRIPTION
+    
+    This program reads messages from the Error Console and sends them
+    to STDOUT.
+    
+    =head1 SEE ALSO
+    
+    L<https://developer.mozilla.org/en/Error_Console>
+    
+    L<https://developer.mozilla.org/en/nsIConsoleService> - the underlying
+    Console Service that also shows how to listen to events getting
+    added.
+    
+    =cut
+
+Download this example: L<http://cpansearch.perl.org/src/CORION/WWW-Mechanize-Firefox-0.27/examples/tail-console.pl>
 
 =head2 Example: urlbar.pl
 
