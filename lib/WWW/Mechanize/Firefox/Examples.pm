55d7f730b7e4b007a68ed71e38d29091e48e51dd
Max Maischein <corion@corion.net>
2010-08-18 17:17:59 +0200

    Regenerate Examples.pm

--- lib/WWW/Mechanize/Firefox/Examples.pm
+++ lib/WWW/Mechanize/Firefox/Examples.pm
@@ -85,7 +85,9 @@ Download this example: L<http://cpansearch.perl.org/src/CORION/WWW-Mechanize-Fir
     use strict;
     use WWW::Mechanize::Firefox;
     
-    my $mech = WWW::Mechanize::Firefox->new();
+    my $mech = WWW::Mechanize::Firefox->new(
+        activate => 1, # bring the tab to the foreground
+    );
     $mech->get('http://www.perlworkshop.de');
     
     <>;
