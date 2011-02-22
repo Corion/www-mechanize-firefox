e9684879ff02ac004750046e6e6e98203634452d
Max Maischein <corion@corion.net>
2011-02-22 18:40:49 +0100

    Bump the versions

--- lib/HTTP/Cookies/MozRepl.pm
+++ lib/HTTP/Cookies/MozRepl.pm
@@ -5,7 +5,7 @@ use parent 'HTTP::Cookies';
 use Carp qw[croak];
 
 use vars qw[$VERSION];
-$VERSION = '0.45';
+$VERSION = '0.46';
 
 =head1 NAME
 
--- lib/WWW/Mechanize/Firefox.pm
+++ lib/WWW/Mechanize/Firefox.pm
@@ -19,7 +19,7 @@ use Encode qw(encode decode);
 use Carp qw(carp croak );
 
 use vars qw'$VERSION %link_spec';
-$VERSION = '0.45';
+$VERSION = '0.46';
 
 =head1 NAME
 
