3fc0ab64c63a81d55ff12c91a8f8bca4de0bc058
Max Maischein <corion@corion.net>
2011-07-24 21:47:07 +0200

    Make Firefox version search path configurable for testing

--- t/helper.pm
+++ t/helper.pm
@@ -2,15 +2,18 @@ package # hide from CPAN indexer
     t::helper;
 use strict;
 use Test::More;
+use File::Glob qw(bsd_glob);
 
 sub firefox_instances {
     my ($filter) = @_;
     $filter ||= qr/^/;
     my @instances;
     push @instances, undef; # default Firefox instance
-    if (-d 'firefox-versions') { # author test with local instances
-        push @instances, sort glob 'firefox-versions/*/FirefoxPortable.exe'; # sorry, Windows-only
-    };
+    
+    # add author tests with local versions
+    my $spec = $ENV{TEST_WWW_MECHANIZE_FIREFOX_VERSIONS}
+             || 'firefox-versions/*/FirefoxPortable*'; # sorry, likely a bad default
+    push @instances, sort {$a cmp $b} grep { -x } bsd_glob $spec;
     
     grep { ($_ ||'') =~ /$filter/ } @instances;
 };
