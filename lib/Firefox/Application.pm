cfbf30320f27c290b69c7ada05bba0c71f360335
Max Maischein <corion@corion.net>
2011-07-29 09:08:58 +0200

    Re-release

--- lib/Firefox/Application.pm
+++ lib/Firefox/Application.pm
@@ -6,7 +6,7 @@ use URI;
 use Carp qw(carp croak);
 
 use vars qw'$VERSION';
-$VERSION = '0.54';
+$VERSION = '0.55';
 
 =head1 NAME
 
--- lib/WWW/Mechanize/Firefox.pm
+++ lib/WWW/Mechanize/Firefox.pm
@@ -18,7 +18,7 @@ use Encode qw(encode decode);
 use Carp qw(carp croak );
 
 use vars qw'$VERSION %link_spec';
-$VERSION = '0.54';
+$VERSION = '0.55';
 
 =head1 NAME
 
