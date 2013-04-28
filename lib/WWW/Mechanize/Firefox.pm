7948582be374905a9168ab8e4028ae99e213cf25
Max Maischein <corion@corion.net>
2013-04-28 18:07:10 +0200

    Added ->post, code and test contributed by dfaure

--- lib/WWW/Mechanize/Firefox.pm
+++ lib/WWW/Mechanize/Firefox.pm
@@ -13,6 +13,7 @@ use Firefox::Application;
 use MozRepl::RemoteObject ();
 use MozRepl::RemoteObject::Methods ();
 use HTTP::Cookies::MozRepl ();
+use HTTP::Request::Common ();
 use Scalar::Util qw'blessed weaken';
 use Encode qw(encode decode);
 use Carp qw(carp croak );
@@ -849,6 +850,100 @@ sub get_local {
     $fn =~ s!\\!/!g; # fakey "make file:// URL"
 
     $self->get("file://$fn", %options);
+}
+
+=head2 C<< $mech->post( $url, %options ) >>
+
+  $mech->post( 'http://example.com',
+      params => { param => "Hello World" },
+      headers => {
+        "Content-Type" => 'application/x-www-form-urlencoded',
+      },
+      charset => 'utf-8',
+  );
+
+Sends a POST request to C<$url>.
+
+A C<Content-Length> header will be automatically calculated if
+it is not given.
+
+The following options are recognized:
+
+=over 4
+
+=item *
+
+C<headers> - a hash of HTTP headers to send. If not given,
+the content type will be generated automatically.
+
+=item *
+
+C<data> - the raw data to send, if you've encoded it already.
+
+=back
+
+=cut
+
+sub post {
+    my ($self, $url, %options) = @_;
+    my $b = $self->tab->{linkedBrowser};
+    $self->clear_current_form;
+
+    my $flags = 0;
+    if ($options{no_cache}) {
+      $flags = $self->repl->constant('nsIWebNavigation.LOAD_FLAGS_BYPASS_CACHE');
+    };
+    if (! exists $options{synchronize}) {
+      $options{synchronize} = $self->events;
+    };
+    if( !ref $options{synchronize}) {
+      $options{synchronize} = $options{synchronize}
+                              ? $self->events
+                              : []
+    };
+
+    # If we don't have data, encode the parameters:
+    if( !$options{ data }) {
+        my $req= HTTP::Request::Common::POST( $url, $options{params} );
+        warn $req->content;
+        $options{ data } = $req->content;
+    };
+
+    $options{ charset } ||= 'utf-8';
+    $options{ headers } ||= {};
+    $options{ headers }->{"Content-Type"} ||= "application/x-www-form-urlencoded";
+    if( $options{ charset }) {
+        $options{ headers }->{"Content-Type"} .= "; charset=$options{ charset }";
+    };
+
+    my $streamPostData = $self->repl->declare(<<'JS');
+      function(headers, dataString) {
+        // POST method requests must wrap the encoded text in a MIME stream
+        const Cc = Components.classes;
+        const Ci = Components.interfaces;
+        var stringStream = Cc["@mozilla.org/io/string-input-stream;1"].
+                           createInstance(Ci.nsIStringInputStream);
+        if ("data" in stringStream) // Gecko 1.9 or newer
+          stringStream.data = dataString;
+        else // 1.8 or older
+          stringStream.setData(dataString, dataString.length);
+
+        var postData = Cc["@mozilla.org/network/mime-input-stream;1"].
+                       createInstance(Ci.nsIMIMEInputStream);
+        for( h in headers ) {
+            postData.addHeader( h, headers[h] );
+        };
+        postData.addContentLength = true;
+        postData.setData(stringStream);
+
+        return postData;
+      }
+JS
+
+    $self->_sync_call($options{synchronize}, sub {
+      my $postData = $streamPostData->($options{headers}, $options{data});
+      $b->loadURIWithFlags(''.$url, $flags, undef, $options{charset}, $postData);
+    });
 }
 
 =head2 C<< $mech->add_header( $name => $value, ... ) >>
@@ -4008,10 +4103,6 @@ C<< ->put >>
 I have no use for it
 
 =item *
-
-C<< ->post >>
-
-I have no use for it
 
 =back
 
---
+++ t/50-mech-post.t
@@ -0,0 +1,44 @@
+#!perl -w
+use strict;
+use Test::More;
+use WWW::Mechanize::Firefox;
+use lib 'inc', '../inc';
+use Test::HTTP::LocalServer;
+
+use t::helper;
+
+if (my $err = t::helper::default_unavailable) {
+    plan skip_all => "Couldn't connect to MozRepl: $@";
+    exit
+} else {
+    plan tests => 8;
+};
+
+my $mech=
+    WWW::Mechanize::Firefox->new(
+        autodie => 0,
+        #log => [qw[debug]],
+    );
+
+my $server = Test::HTTP::LocalServer->spawn(
+    #debug => 1
+);
+
+isa_ok $mech, 'WWW::Mechanize::Firefox';
+
+my ($site,$estatus) = ($server->url,200);
+my $res = $mech->post($site, params => { query => 'queryValue1', query2 => 'queryValue2' });
+isa_ok $res, 'HTTP::Response', "Response";
+
+is $mech->uri, $site, "Navigated to $site";
+
+is $res->code, $estatus, "POSTting $site returns HTTP code $estatus from response"
+    or diag $mech->content;
+
+is $mech->status, $estatus, "POSTting $site returns HTTP status $estatus from mech"
+    or diag $mech->content;
+
+ok $mech->success, 'We consider this response successful';
+
+like $mech->content, qr/queryValue1/, "We find our parameter 'query'";
+like $mech->content, qr/queryValue2/, "We find our parameter 'query2'";
