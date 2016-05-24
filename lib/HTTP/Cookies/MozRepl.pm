52d18b44b4d327eb8968548d23556a5925be872e
Max Maischein <corion@corion.net>
2016-05-24 21:40:50 +0200

    Bump version

--- lib/HTTP/Cookies/MozRepl.pm
+++ lib/HTTP/Cookies/MozRepl.pm
@@ -6,7 +6,7 @@ use parent 'HTTP::Cookies';
 use Carp qw[croak];
 
 use vars qw[$VERSION];
-$VERSION = '0.78';
+$VERSION = '0.79';
 
 =head1 NAME
 
--- lib/WWW/Mechanize/Firefox.pm
+++ lib/WWW/Mechanize/Firefox.pm
@@ -19,7 +19,7 @@ use Encode qw(encode decode);
 use Carp qw(carp croak );
 
 use vars qw'$VERSION %link_spec @CARP_NOT';
-$VERSION = '0.78';
+$VERSION = '0.79';
 @CARP_NOT = ('MozRepl::RemoteObject',
              'MozRepl::AnyEvent',
              'MozRepl::RemoteObject::Instance'
